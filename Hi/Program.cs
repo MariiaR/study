e285d9dc4d56ccb3e53e21003c5c0e7039d341e0
vitaly-maximov <g.vitaly.maximov@gmail.com>
2016-05-04 18:23:41 +0300

    no message

--- Hi/Program.cs
+++ Hi/Program.cs
@@ -10,12 +10,21 @@ namespace Hi
     {
         static void Main(string[] args)
         {
-            string hello = "What is your name?";
-            string name;
-            Console.WriteLine(hello);
-            name = Console.ReadLine();
-            string answer = "Hi,";
-            Console.WriteLine(answer + name);
+            // некоторые переменные только усложняют чтение программы, можно обойтись и без них
+            Console.WriteLine("What is your name?");
+
+            // здесь переменная позволяет лучше понять смысл программы
+            string name = Console.ReadLine();
+
+            // Console.WriteLine(answer + name); так лучше не писать. Прочитай материал из книжки про string.Format(...) и по ссылке
+            // https://msdn.microsoft.com/en-us/library/system.string.format(v=vs.110).aspx
+
+            Console.WriteLine("Hi {0}!", name);
+
+            // Можно было написать
+            // Console.WriteLine("Hi {0}!", Console.ReadLine());
+            // не объявляя name, но это ухудшает читабельность
+
             Console.ReadLine();
         }
     }
