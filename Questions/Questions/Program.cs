45e68fde3effd362acd8de0def65da479b85095f
MariiaR <mariarogonskaya@gmail.com>
2016-05-05 20:30:38 +0300

    Questions

---
+++ Questions/Questions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your surname?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("When were you born?");
+            //string birthDate = Console.ReadLine();
+            //birthDate
+            
+        }
+    }
+}
