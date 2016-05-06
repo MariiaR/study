256de3b5e4459da0a347b73a0bfe7402ee050e5a
MariiaR <mariarogonskaya@gmail.com>
2016-05-06 16:35:00 +0300

    NameSurname

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your surname?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("When were you born?");
+            DateTime birthDate = Console.ReadLine().ToString;
+            Console.WriteLine("What is your phone number?");
+            string phone = Console.ReadLine();
+            Console.WriteLine("What is your home address?");
+            string address = Console.ReadLine();
+            Console.WriteLine("Dear {0},", name)          
+
+        }
+    }
+}
---
+++ Questions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your surname?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("When were you born?");
+            DateTime birthDate = DateTime.ParseExact("05261992", "mmddyyyy", null);
+            Console.ReadLine();
+            Console.WriteLine("What is your phone number?");
+            string phone = Console.ReadLine();
+            Console.WriteLine("What is your home address?");
+            string address = Console.ReadLine();
+            Console.WriteLine("Dear {0} {1}", name, surname);
+            Console.ReadLine();
+            
+        }
+    }
+}
