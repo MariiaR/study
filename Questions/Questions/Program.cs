c5bd643e6e389336e86e8fd8271b5bbf990246a9
vitaly-maximov <g.vitaly.maximov@gmail.com>
2016-05-31 20:15:00 +0300

    no message

--- Questions/Questions/Program.cs
+++ Questions/Questions/Program.cs
@@ -9,28 +9,46 @@ namespace Questions
 {
     class Program
     {
+        // Ты очень хорошо все сделала! Молодец!
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
+
             Console.WriteLine("What is your surname?");
             string surname = Console.ReadLine();
+
             Console.WriteLine("When were you born?");
             string birthDate = Console.ReadLine();
-            //birthDate = DateTime.Parse(birthDate).ToShortDateString();
-            int yearNow = DateTime.Today.Year;
-            int birthYear = DateTime.Parse(birthDate).Year;
-            int birthMonth = DateTime.Parse(birthDate).Month;
-            int birthDay = DateTime.Parse(birthDate).Day;
-            int age = yearNow - birthYear;
+
+            // ты делаешь одну и ту же операцию 3 раза DateTime.Parse(birthDate)
+            // в данном случае - это не страшно, но такие операции могут быть очень долгими, да и просто это дурной тон
+
+            //int birthYear = DateTime.Parse(birthDate).Year;
+            //int birthMonth = DateTime.Parse(birthDate).Month;
+            //int birthDay = DateTime.Parse(birthDate).Day;
+
+            // лучше написать так
+            DateTime birthDateTime = DateTime.Parse(birthDate);
+
+            // и везде использовать новую переменную, например: birthDateTime.Day
+            int age = DateTime.Today.Year - birthDateTime.Year;
+            
             Console.WriteLine("What is your phone number?");
             string phone = Console.ReadLine();
+
             Console.WriteLine("What is your home address?");
             string address = Console.ReadLine();
-            //Console.WriteLine(Age.ToString("dd:MM:yyyy"));
-            Console.WriteLine("Dear {0} {1}, {2}.{3}.{4} you will have your {5} birthday", name, surname, birthDay, birthMonth, yearNow, age);
-            Console.ReadLine();
 
+            //Console.WriteLine("Dear {0} {1}, {2}.{3}.{4} you will have your {5} birthday", name, surname, birthDay, birthMonth, yearNow, age);
+
+            // DateTime имеет множество способов форматирования, например: {0:MM:ss} выведет месяц и секунды
+            Console.WriteLine("Dear {0} {1}, {2:dd.MM.yyyy} you will have your {3} birthday", name, surname, birthDateTime, age);
+
+            // что если пользователь введет неверную дату?
+            // почитай про try catch и попробуй обработать неверный ввод (подсказка: используй цикл "пока пользователь не ввел нормальную дату")
+
+            Console.ReadLine();
         }
     }
 }
